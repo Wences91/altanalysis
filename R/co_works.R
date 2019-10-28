6967a4f2ac0bc6760460232b2d9afc8aea037bac
Wences91 <wences91@gmail.com>
2019-10-28 22:45:39 +0100

    Create co_works.R

---
+++ R/co_works.R
@@ -0,0 +1,12 @@
+#' co_works
+#' 
+#' @param publications_titles titles of publications
+#' @export
+#' @import NLP
+#' @import tm
+#' @import openNLP
+#' @import openNLPmodels.en
+
+
+co_works <- function(publications_titles){
+}
