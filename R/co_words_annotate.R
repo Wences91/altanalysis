b5fd562302b7b789d42c4ddd171fb95e8759eb36
Wences91 <wences91@gmail.com>
2019-10-29 17:05:54 +0100

    Create co_words_annotate.R

---
+++ R/co_words_annotate.R
@@ -0,0 +1,36 @@
+#' co_words_annotatde
+#' 
+#' @param publications_titles titles of publications
+#' @export
+#' @import NLP
+#' @import tm
+#' @importFrom openNLP Maxent_Sent_Token_Annotator Maxent_Word_Token_Annotator
+
+# recommendation
+#options(java.parameters = "- Xmx3000m")
+
+# function 1 (annotate) all)
+co_words_annotatde <- function(publications_titles){
+  # clean data
+  publications_titles <- unique(publications_titles[,which(names(publications_titles) %in% c('Research Output Title', 'Details Page URL'))])
+  #todo: test no duplicated
+  
+  # preprocess
+  publications_titles$`Research Output Title` <- gsub("[^-0-9A-Za-z///,/./: ]", '', publications_titles$`Research Output Title`, perl = TRUE)
+  publications_titles$`Research Output Title` <- gsub("web of science", 'WOS', ignore.case = TRUE, publications_titles$`Research Output Title`)
+  
+  
+  annotate_titles <- list()
+  # identify all words in the titles
+  sapply(1:dim(publications_titles)[1], function(i){
+    print(i)
+    # at first I transform the title to lower case
+    text <- tolower(publications_titles$`Research Output Title`[i])
+    annotate_titles[[i]] <<- list(title = text,
+                                  id = publications_titles$`Details Page URL`[i],
+                                  annotate = NLP::annotate(text, list(openNLP::Maxent_Sent_Token_Annotator(), openNLP::Maxent_Word_Token_Annotator())))
+    gc()
+    })
+  
+  return(annotate_titles)
+}
