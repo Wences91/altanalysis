8fcb8de80583661ba59f2b62fa54357484a664dd
Wences91 <wences91@gmail.com>
2019-11-04 01:13:56 +0100

    Update co_words_tags.R

--- R/co_words_tags.R
+++ R/co_words_tags.R
@@ -13,28 +13,42 @@
 co_words_tags <- function(annotate_titles){
   # assing tags to each word
   invisible(sapply(1:length(annotate_titles), function(i){
-    print(i)
+    
+    cat('\r', i)
+    
     # get tags
-    pos_res <- NLP::annotate(annotate_titles[[i]]$title, openNLP::Maxent_POS_Tag_Annotator(language = 'en'), annotate_titles[[i]]$annotate)
-    word_subset <- subset(pos_res, type == 'word')
-    tags <- sapply(word_subset$features , '[[', "POS")
-    
-    annotate_titles[[i]]$tagged_words <<- data.frame(word = as.character(),
-                                                     tag = as.character(),
-                                                     stringsAsFactors = FALSE)
-    
-    # add word-tags
-    sapply(1:length(word_subset), function(x){
-      word <- substr(annotate_titles[[i]]$title, word_subset$start[x], word_subset$end[x])
-      tag <- tags[x]
-      annotate_titles[[i]]$tagged_words <<- rbind.data.frame(annotate_titles[[i]]$tagged_words, data.frame(word = word,
-                                                                                                          tag = tag,
-                                                                                                          stringsAsFactors = FALSE),
-                                                             stringsAsFactors = FALSE)
-    })
+    if(all(is.na(annotate_titles[[i]]$annotate))){
+      annotate_titles[[i]]$tagged_words <<- NA
+    }else{
+      tryCatch(
+        {
+          pos_res <- NLP::annotate(annotate_titles[[i]]$title, openNLP::Maxent_POS_Tag_Annotator(language = 'en'), annotate_titles[[i]]$annotate)
+          word_subset <- subset(pos_res, type == 'word')
+          tags <- sapply(word_subset$features , '[[', "POS")
+          
+          annotate_titles[[i]]$tagged_words <<- data.frame(word = as.character(),
+                                                           tag = as.character(),
+                                                           stringsAsFactors = FALSE)
+          
+          # add word-tags
+          sapply(1:length(word_subset), function(x){
+            word <- substr(annotate_titles[[i]]$title, word_subset$start[x], word_subset$end[x])
+            tag <- tags[x]
+            annotate_titles[[i]]$tagged_words <<- rbind.data.frame(annotate_titles[[i]]$tagged_words, data.frame(word = word,
+                                                                                                                 tag = tag,
+                                                                                                                 stringsAsFactors = FALSE),
+                                                                   stringsAsFactors = FALSE)
+          })
+          
+        },error=function(e){
+          message('No tags found')
+          annotate_titles[[i]]$tagged_words <<- NA
+        }
+      )
+    }
     
     gc()
-  }))
+    }))
   
   return(annotate_titles)
 }
