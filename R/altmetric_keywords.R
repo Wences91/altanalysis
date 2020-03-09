e45d98e9ffb5d93a9d47c59185d7c0bd2fdb858b
Wences91 <wences91@gmail.com>
2020-03-09 23:38:42 +0100

    Update altmetric_keywords.R

--- R/altmetric_keywords.R
+++ R/altmetric_keywords.R
@@ -1,48 +1,50 @@
 #' altmetric_keywords
 #' 
-#' @param altmetric_wos_mentions Altmetric.com mentions with WoS data
-#' @param wos_records WoS records
-#' @param remove_journals name of journals to remove
+#' @param altmetric_wos_mentions Altmetric.com mentions with acccession number data.frame
+#' @param wos_records WoS records data.frame
+#' @param remove_journals Character o vector of optional names of journals to remove from
+#' @description This function get Altmetric.com mentions (the result from altmetric_wos_mentions) and WoS records. Then it splits all WoS records into author keywords (one per record) and join with Altmetric.com mentions. It returns a list with two data.frame: all Altmetric.com mentions that have author keywords and these, and the same with records.
 #' @export
 #' @importFrom dplyr inner_join
 #' 
 
 
 altmetric_keywords <- function(altmetric_wos_mentions, wos_records, remove_journals=NULL){
+  
   # Check integrity
   if(!(all(wos_records$UT %in% altmetric_wos_mentions$`Accession Number`) & all(altmetric_wos_mentions$`Accession Number` %in% wos_records$UT))){
     stop('Some Altmetric or WoS records are not included')
   }
   
-  # Create a empty data.frame to add al keywords
+  # Create a empty data.frame to gather all keywords
   keys <- data.frame(id=character(), keyword=character(), stringsAsFactors = FALSE)
-  
-  ### Select accession number (UT) and authors keywords (DE)
 
-  # remove journals
+  # Remove journals
   if(!is.null(remove_journals)){
     keywords <- wos_records[which(!(wos_records$SO %in% remove_journals)),]
   }
+  
+  # Select accession number (UT) and authors keywords (DE)
   keywords <- keywords[, c('UT','DE')]
   keywords <- keywords[which(keywords$DE!=''),]
   
-  # keywords to lower to reduce problems
+  # Keywords to lower to reduce problems
   keywords$DE <- tolower(keywords$DE)
   
-  # split keywords
+  # Split keywords
   sapply(1:dim(keywords)[1], function(x){
     aux <- strsplit(keywords$DE[x], '; ')[[1]]
     keys <<- rbind.data.frame(data.frame(id=keywords$UT[x], keyword=aux, stringsAsFactors = FALSE), keys,
                               stringsAsFactors = FALSE)
   })
   
-  # remove 0 from web 2.0 and similars
+  # Remove 0 from web 2.0 and similars
   keys <- keys[which(!(keys$keyword == '0')),]
   
-  # remove ' and "
+  # Remove ' and "
   keys$keyword <- gsub( '"|\'', '', keys$keyword)
   
-  # remove some duplicated keywords in the same records
+  # Remove some duplicated keywords in the same records
   keys <- unique(keys)
   
   keys_mentions <- dplyr::inner_join(keys, altmetric_wos_mentions, by=c('id'='Accession Number'))
